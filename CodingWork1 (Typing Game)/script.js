b2cfed44fbb68aa790eb98d3bca0cacb2f27d0b4
GabrielYuLunLou <74881699+GabrielYuLunLou@users.noreply.github.com>
2020-12-13 15:28:54 -0800

    Add files via upload

---
+++ CodingWork1 (Typing Game)/script.js
@@ -0,0 +1,31 @@
+const words = ['happy', 'ball', 'warlike', 'dependent', 'silver', 'superficial', 'telecommunication', 'juice'];
+let score = 0;
+let time = 10;
+let randomWord;
+
+function generateRandomWord() {
+  let randomNumber = Math.floor(Math.random() * words.length); 
+  randomWord = words[randomNumber];
+  $('#word').html(randomWord);
+}
+
+generateRandomWord()
+
+setInterval(() => {
+    time = time - 1;
+    $('#time').html(time);
+    if (time === 0) {
+        alert('Game Over')
+    }
+}, 1000);
+
+$('#text').on('input', (e) => {
+  if (randomWord === e.target.value) {
+    generateRandomWord()
+    e.target.value = '';
+    score = score + 1
+    $('#score').html(score);
+    time = time + 5
+    $('#time').html(time);
+  }
+})
